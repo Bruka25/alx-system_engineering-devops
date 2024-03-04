e57b811bf23e60b58ec3a5b8a5e295ab699b09d4
Bruka25 <bkteshome862@gmail.com>
2024-03-05 00:23:32 +0300

    configuration of http header using puppet automation second trial fix

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,38 +1,7 @@
 # Update package lists
-package { 'nginx-common':
-  ensure   => latest,
-  provider => 'apt',
-}
-
-# Install nginx
 package { 'nginx':
   ensure   => latest,
-  require  => Package['nginx-common'],
-}
-
-# Create directory for HTML files
-file { '/etc/nginx/html':
-  ensure => directory,
-}
-
-# Create index.html file with "Hello World!" content
-file { '/etc/nginx/html/index.html':
-  ensure  => present,
-  content => "Hello World!\n",
-}
-
-# Create 404.html file with "Ceci n'est pas une page" content
-file { '/etc/nginx/html/404.html':
-  ensure  => present,
-  content => "Ceci n'est pas une page\n",
-}
-
-# Configure Nginx server
-file { '/etc/nginx/sites-available/default':
-  ensure  => present,
-  content => template('nginx/default.conf.erb'),
-  require => Package['nginx'],
-  notify  => Service['nginx'],
+  provider => 'apt',
 }
 
 # Define Nginx service
@@ -42,25 +11,34 @@ service { 'nginx':
   require => Package['nginx'],
 }
 
-# Template for Nginx configuration file
-file { '/etc/nginx/sites-available/default':
+# Create directory for Nginx configuration files
+file { '/etc/nginx/conf.d':
+  ensure => directory,
+}
+
+# Create Nginx configuration file for custom HTTP header
+file { '/etc/nginx/conf.d/custom_headers.conf':
   ensure  => present,
   content => @(EOT),
+# Custom HTTP headers configuration
 server {
-    listen 80;
+    listen 80 default_server;
     listen [::]:80 default_server;
-    root   /etc/nginx/html;
-    add_header X-Served-By <%= @hostname %>;
-    index  index.html index.htm;
 
-    location /redirect_me {
-        return 301 http://www.youtube.com/;
+    # Custom HTTP header X-Served-By with the hostname
+    add_header X-Served-By <%= @hostname %>;
+
+    # Serve a simple "Hello World!" page
+    location / {
+        return 200 'Hello World!';
+        add_header Content-Type text/plain;
     }
 
+    # Error page configuration
     error_page 404 /404.html;
-    location /404 {
-      root /etc/nginx/html;
-      internal;
+    location = /404.html {
+        root /usr/share/nginx/html;
+        internal;
     }
 }
 EOT
