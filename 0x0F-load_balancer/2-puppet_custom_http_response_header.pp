241f08aa9984ae77e2468f0aa734e772c4a6c0bf
Bruka25 <bkteshome862@gmail.com>
2024-03-05 00:29:04 +0300

    configuration of http header using puppet automation third trial fix

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,47 +1,8 @@
-# Update package lists
-package { 'nginx':
-  ensure   => latest,
-  provider => 'apt',
-}
-
-# Define Nginx service
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => Package['nginx'],
-}
-
-# Create directory for Nginx configuration files
-file { '/etc/nginx/conf.d':
-  ensure => directory,
-}
-
-# Create Nginx configuration file for custom HTTP header
-file { '/etc/nginx/conf.d/custom_headers.conf':
-  ensure  => present,
-  content => @(EOT),
-# Custom HTTP headers configuration
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    # Custom HTTP header X-Served-By with the hostname
-    add_header X-Served-By <%= @hostname %>;
-
-    # Serve a simple "Hello World!" page
-    location / {
-        return 200 'Hello World!';
-        add_header Content-Type text/plain;
-    }
-
-    # Error page configuration
-    error_page 404 /404.html;
-    location = /404.html {
-        root /usr/share/nginx/html;
-        internal;
-    }
-}
-EOT
-  require => Package['nginx'],
-  notify  => Service['nginx'],
+# puppet script that creates a custom HTTP header responset
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }
