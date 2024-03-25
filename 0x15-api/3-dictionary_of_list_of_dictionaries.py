e3791b4d4ffca11fb0587e96617e56f07d6a74c4
Bruka25 <bkteshome862@gmail.com>
2024-03-25 21:04:37 +0300

    python script that uses REST API to to export data in the JSON format

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -8,24 +8,15 @@ import json
 import requests
 
 if __name__ == "__main__":
-    users = requests.get("http://jsonplaceholder.typicode.com/users").json()
-    tasks = requests.get("http://jsonplaceholder.typicode.com/todos").json()
-    storage = {}
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
 
-    for user in users:
-        emp_id = user.get("id")
-        username = user.get("username")
-        all_tasks = []
-
-        for task in tasks:
-            if (task.get("userId") == emp_id and task.get("completed")):
-                temp = {}
-                temp["task"] = task.get("title")
-                temp["completed"] = task.get("completed")
-                temp["username"] = username
-                all_tasks.append(temp)
-
-        storage[emp_id] = all_tasks
-
-    with open("todo_all_employees.json", 'w+') as jsonfile:
-        json.dump(storage, jsonfile)
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
