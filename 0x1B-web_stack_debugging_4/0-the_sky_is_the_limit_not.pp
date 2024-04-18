9163c35f053701552a2a760214dddd464872613f
Bruka25 <bkteshome862@gmail.com>
2024-04-18 20:09:19 +0300

    readme and task one for fixing our stack so that we get to 0, and remember that when something is going wrong, logs are your best friends

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,4 @@
+# puppet file for Increasing the amount of traffic an Nginx server can handle
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
